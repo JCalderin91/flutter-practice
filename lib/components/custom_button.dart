cd1f48e6c8dc0196910bc29c9f2b29c28a21456d
Jesus Calderin <jesuscalderin91@gmail.com>
2021-03-04 17:24:36 -0400

    feat: login page

--- lib/components/custom_button.dart
+++ lib/components/custom_button.dart
@@ -6,21 +6,21 @@ class CustomButtom extends StatelessWidget {
   final Function onPressed;
   final Color color;
   final Color textColor;
-  final String text;
   final Widget child;
+  final double width;
   const CustomButtom({
     Key key,
     @required this.onPressed,
     this.color = kPrimaryColor,
     this.textColor = Colors.white,
-    @required this.text,
     @required this.child,
+    this.width = double.infinity,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      width: double.infinity,
+      width: width,
       child: FlatButton(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(50),
--- lib/pages/login/login_screen.dart
+++ lib/pages/login/login_screen.dart
@@ -15,126 +15,121 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: SizeConfig.screenHeight * 0.40,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.3),
-                  offset: Offset(0, 0),
-                  blurRadius: 30,
-                )
-              ],
-              image: DecorationImage(
-                image: AssetImage('assets/images/wallpaper.jpeg'),
-                fit: BoxFit.cover,
-                colorFilter: ColorFilter.mode(
-                  Colors.black.withOpacity(0.5),
-                  BlendMode.hardLight,
+      body: SingleChildScrollView(
+        // controller: controller,
+        child: Column(
+          children: [
+            Container(
+              height: SizeConfig.screenHeight * 0.40,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    offset: Offset(0, 0),
+                    blurRadius: 30,
+                  )
+                ],
+                image: DecorationImage(
+                  image: AssetImage('assets/images/wallpaper.jpeg'),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.5),
+                    BlendMode.hardLight,
+                  ),
+                ),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(40),
+                  bottomRight: Radius.circular(40),
                 ),
               ),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(40),
-                bottomRight: Radius.circular(40),
+              child: SafeArea(
+                child: Column(
+                  children: [
+                    const SizedBox(height: 30),
+                    Text(
+                      'Delivery App',
+                      style: TextStyle(
+                        color: Colors.white60,
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Spacer(),
+                    Text(
+                      'Iniciar Sesión',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                  ],
+                ),
               ),
             ),
-            child: SafeArea(
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
               child: Column(
                 children: [
-                  const SizedBox(height: 30),
+                  CutomTextfield(
+                      hint: 'Correo elétronico', icon: Icon(Icons.people_alt)),
+                  CutomTextfield(hint: 'Contraseña', icon: Icon(Icons.lock)),
+                  const SizedBox(height: 5),
+                  CustomButtom(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => HomePage(),
+                        ),
+                      );
+                    },
+                    child: Text(
+                      'Iniciar sesión',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Divider(),
+                  // CustomButtom(
+                  //   onPressed: () {},
+                  //   color: Colors.white,
+                  //   textColor: kTextColor,
+                  //   width: 40,
+                  //   child: Image.asset(
+                  //     'assets/images/search.png',
+                  //     height: 18,
+                  //   ),
+                  // ),
+                  // const SizedBox(height: 5),
                   Text(
-                    'Delivery App',
+                    'Si no tienes cuenta',
                     style: TextStyle(
-                      color: Colors.white60,
-                      fontSize: 40,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const Spacer(),
-                  Text(
-                    'Iniciar Sesión',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18,
+                  const SizedBox(height: 5),
+                  CustomButtom(
+                    onPressed: () {},
+                    color: Colors.black45,
+                    // textColor: kTextColor,
+                    child: Text(
+                      'Registarte aqui',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                  const SizedBox(height: 15),
                 ],
               ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 30),
-            child: Column(
-              children: [
-                CutomTextfield(
-                    hint: 'Correo elétronico', icon: Icon(Icons.people_alt)),
-                CutomTextfield(hint: 'Contraseña', icon: Icon(Icons.lock)),
-                const SizedBox(height: 5),
-                CustomButtom(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => HomePage(),
-                      ),
-                    );
-                  },
-                  text: 'Ingresar',
-                  child: Text(
-                    'Ingresar',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 5),
-                CustomButtom(
-                  onPressed: () {
-                    // Navigator.push(
-                    //   context,
-                    //   MaterialPageRoute(
-                    //     builder: (context) => HomePage(),
-                    //   ),
-                    // );
-                  },
-                  color: Colors.white,
-                  textColor: kTextColor,
-                  text: 'Iniciar sesión con google',
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Image.asset(
-                        'assets/images/search.png',
-                        height: 18,
-                      ),
-                      const SizedBox(width: 20.0),
-                      Text(
-                        'Ingresar con Google',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                Text(
-                  '¿Olvidaste tu contraseña?',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 20),
-              ],
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
